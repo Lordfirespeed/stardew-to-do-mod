7e9682378f1bacc298aaaad3f200e8e15ff5711e
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-04-15 20:19:34 +0100

    use a `KeybindList`

--- ModConfig.cs
+++ ModConfig.cs
@@ -5,24 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework.Input;
-
+using StardewModdingAPI.Utilities;
 
 namespace ToDoMod
 {
     class ModConfig
     {
-        public string OpenListKey { get; set; }
+        public KeybindList ToggleListKey { get; set; } = KeybindList.Parse("F2");
 
-        public bool UseLargerFont { get; set; }
+        public bool UseLargerFont { get; set; } = false;
 
-        public bool OpenAtStartup { get; set; }
-
-        public ModConfig()
-        {
-            this.OpenListKey = Keys.F2.ToString();
-            this.UseLargerFont = false;
-            this.OpenAtStartup = false;
-        }
-
+        public bool OpenAtStartup { get; set; } = false;
     }
 }
